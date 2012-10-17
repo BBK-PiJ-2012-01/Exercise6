9c5b7ed2c8bd8cd7493eadbe5b6b12e469a864f6
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-10-17 12:31:33 +0100

    Initial commit, including a copy of common files from exercise 5.

---
+++ src/BBK/PiJ01/exercise6/Exercise6.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BBK.PiJ01.exercise6;
+
+/**
+ *
+ * @author Sam Wright <swrigh11@dcs.bbk.ac.uk>
+ */
+public class Exercise6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
